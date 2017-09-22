c317e3e8ea57512765541c3125c713955c9fa6b6
xh <xxhhao@126.com>
2017-09-22 18:31:48 +0800

    notebook

--- utils.js
+++ utils.js
@@ -3,19 +3,42 @@
  */
 
 $(function(){
-    $(":button").click(function(){
-        myForm1();
+    $(".check").click(function(){
+        myInput1();
+        myInput2();
+        myInput3();
     });
 });
 
 //必填项验证
-function myForm1(){
-    var x=$(".myForm1").find(".myInput1");
+function myInput1(){
+    var x=$(".myInput1");
     if(x.val()==""){
-        //return false
-        alert("wrong")
+        return false
     }
     else{
-        alert("ng")
+        return true
+    }
+}
+
+//数字验证
+function myInput2(){
+    var x=$(".myInput2");
+    if(isNaN(x.val())){
+        return false
+    }
+    else{
+        return true
+    }
+}
+
+//英文字母验证
+function myInput3(){
+    var x=$(".myInput3");
+    if(!x.val().match(/^[A-Za-z]+$/)){
+        alert("www")
+    }
+    else{
+        return true
     }
 }
