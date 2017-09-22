52d8a23190893df710c99d83454c2203a9980f48
xh <xxhhao@126.com>
2017-09-22 17:59:40 +0800

    notebook

--- utils.js
+++ utils.js
@@ -2,7 +2,20 @@
  * Created by Administrator on 2017/9/22.
  */
 
-//必填验证
-function my-form1(my-input){
-    var x=$(".my-form1>.my-input1")
+$(function(){
+    $(":button").click(function(){
+        myForm1();
+    });
+});
+
+//必填项验证
+function myForm1(){
+    var x=$(".myForm1").find(".myInput1");
+    if(x.val()==""){
+        //return false
+        alert("wrong")
+    }
+    else{
+        alert("ng")
+    }
 }
