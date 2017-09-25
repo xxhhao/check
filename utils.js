9359c8af0f9ba76a854e5139eb0446046edd28cb
xh <xxhhao@126.com>
2017-09-25 15:40:36 +0800

    notebook

--- utils.js
+++ utils.js
@@ -2,16 +2,28 @@
  * Created by Administrator on 2017/9/22.
  */
 
-$(function(){
-    $(".check").click(function(){
-        console.log()
-    });
-});
+//$(function(){
+//    $(".check").click(function(){
+//        Input1()
+//    });
+//});
+//
+//
+//function Input1(){
+//    var x=$(".myInput1").val();
+//    if(x.match(/^1[3-5,8][0-9]{9}$/)){
+//        alert("abc")
+//    }
+//    else {
+//        alert("cba")
+//    }
+//}
+//
+
 
 
 var validate={
-    isEmpty:function(){
-        var x=this.val();
+    isEmpty:function(x){
         if(x==""){
             return true
         }
@@ -20,7 +32,7 @@ var validate={
         }
     },
 
-    isNumber:function(){
+    isNumber:function(x){
         if(!isNaN(x)){
             return true
         }
@@ -29,7 +41,7 @@ var validate={
         }
     },
 
-    isLetter: function () {
+    isLetter: function (x) {
         if(x.match(/^[A-Za-z]$/)){
             return true
         }
@@ -38,7 +50,7 @@ var validate={
         }
     },
 
-    isTelephone:function(){
+    isTelephone:function(x){
         if(x.match(/^1[3-5,8][0-9]{9}$/)){
             return true
         }
@@ -47,7 +59,7 @@ var validate={
         }
     },
 
-    isEmail:function(){
+    isEmail:function(x){
         if(x.match(/^[A-Za-z0-9_-]+@[A-Za-z0-9_-]+(\.[A-Za-z]{2,3}){1,2}$/)){
             return true
         }
@@ -56,8 +68,8 @@ var validate={
         }
     },
 
-    isChinese:function(){
-        if(x.match(/^[\u2E80-\u9FFF]$/)){
+    isChinese:function(x){
+        if(x.match(/^[\u4e00-\u9fa5]$/)){
             return true
         }
         else{
@@ -65,7 +77,7 @@ var validate={
         }
     },
 
-    isPassward:function(){
+    isPassward:function(x){
         if(x.match(/^[\w]{6,20}$/)){
             return true
         }
@@ -74,7 +86,7 @@ var validate={
         }
     },
 
-    isUsername:function(){
+    isUsername:function(x){
         if(x.match(/^[\w]{3,20}$/)){
             return true
         }
@@ -83,5 +95,34 @@ var validate={
         }
     },
 
-    is
+    isMoney:function(x){
+        if(x.match(/^[0-9]*\.[0-9]{2}$/)){
+            return true
+        }
+        else{
+            return false
+        }
+    },
+
+    isIdcard:function(x){
+      if(x.length=18&& x.match(/^[0-9]{6}[1,2][0-1,9][0-9]{2}[0,1][0-9][0-3][0-9]{4}[0-9,x]$/)){
+          return true
+      }
+      else{
+          return false
+      }
+    },
+
+
 };
+
+
+
+
+$(function(){
+    var vD=Object.create(validate);
+    $("#btn1").click(function(){
+        var y=$(".empty").val()
+        vD.isEmpty(y)
+    })
+})
